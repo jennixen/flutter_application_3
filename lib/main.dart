3ac7cdf4849b1eb5ed7b4c02b1090bccf6e0fd5f
Starlumine <jchan1star@gmail.com>
2025-02-10 16:54:15 -0600

    Jessica was here

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'JenniferWuzHere'),
+      home: const MyHomePage(title: 'JenniferWuzHere JessicaWasHere'),
     );
   }
 }
