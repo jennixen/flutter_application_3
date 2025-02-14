d68ac5792b749777488db0e402ca16b28212f2f8
jennixen <143552870+jennixen@users.noreply.github.com>
2025-02-13 19:41:59 -0600

    merged pages

--- lib/main.dart
+++ lib/main.dart
@@ -9,27 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -41,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -57,81 +32,262 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: <Widget>[
+          // The main content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 60),
+                const Text(
+                  'Mixing... mixing... mixing...',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  height: 400,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Positioned(
+                        top: -30,
+                        child: Image.asset(
+                          'assets/wizard.png',
+                          width: 300,
+                          height: 300,
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/cauldron.png',
+                        width: 300,
+                        height: 300,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+
+          // Positioned "Next" button at the bottom right
+          Positioned(
+            bottom: 30,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigate to the next page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const NextPage(),
+                  ),
+                );
+              },
+              child: const Text("Next"),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ChickensPage()),
-        );
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+// Next page with same content + dog image on the right side
+class NextPage extends StatefulWidget {
+  const NextPage({super.key});
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Next Page'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          // The main content with images
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 60),
+                const Text(
+                  'Oh hi doggy!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Stack widget for images
+                SizedBox(
+                  height: 400,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      // Wizard and cauldron remain centered
+                      Positioned(
+                        top: -30,
+                        child: Image.asset(
+                          'assets/wizard.png',
+                          width: 300,
+                          height: 300,
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/cauldron.png',
+                        width: 300,
+                        height: 300,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            right: -200,
+            bottom: -200, 
+            child: Image.asset(
+              'assets/dog.png',
+              width: 1500, 
+              height: 1500,
+            ),
+          ),
+
+          Positioned(
+            bottom: 30,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigate to the next page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const WaitOhNoPage(),
+                  ),
+                );
+              },
+              child: const Text("Next"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// New page with dialogue "Wait oh no!"
+class WaitOhNoPage extends StatefulWidget {
+  const WaitOhNoPage({super.key});
+
+  @override
+  State<WaitOhNoPage> createState() => _WaitOhNoPageState();
+}
+
+class _WaitOhNoPageState extends State<WaitOhNoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Wait Oh No!'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          // The main content with images
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 60),
+                const Text(
+                  'Wait oh no!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                // Stack widget for images
+                SizedBox(
+                  height: 400,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      // Wizard and cauldron remain centered
+                      Positioned(
+                        top: -30,
+                        child: Image.asset(
+                          'assets/wizard.png',
+                          width: 300,
+                          height: 300,
+                        ),
+                      ),
+                      Image.asset(
+                        'assets/cauldron.png',
+                        width: 300,
+                        height: 300,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            right: -200,
+            bottom: -200, 
+            child: Image.asset(
+              'assets/dog.png',
+              width: 1500, 
+              height: 1500,
+            ),
+          ),
+
+          Positioned(
+            bottom: 30,
+            right: 20,
+            child: ElevatedButton(
+              onPressed: () {
+                // Navigate to the next page (if any)
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const WaitOhNoPage(),
+                  ),
+                );
+              },
+              child: const Text("Next"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
 class ChickensPage extends StatefulWidget {
+  const ChickensPage({super.key});
+
   @override
   _ChickensPageState createState() => _ChickensPageState();
 }
@@ -154,7 +310,7 @@ class _ChickensPageState extends State<ChickensPage> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {
         screenWidth = MediaQuery.of(context).size.width;
         screenHeight = MediaQuery.of(context).size.height;
